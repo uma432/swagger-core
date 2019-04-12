fba961058266371e2069b53acdef26b79fa62f60
Rafael Lopez <ralphdoe12@gmail.com>
2019-04-11 23:50:20 -0500

    Ticket 3169 Tests: ApiResponse in ApiResponses annotation, ApiResponse in Callbacks

--- modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/annotations/responses/ApiResponseTypeSchemaTicket3169Test.java
+++ modules/swagger-jaxrs2/src/test/java/io/swagger/v3/jaxrs2/annotations/responses/ApiResponseTypeSchemaTicket3169Test.java
@@ -2,14 +2,16 @@ package io.swagger.v3.jaxrs2.annotations.responses;
 
 import io.swagger.v3.jaxrs2.annotations.AbstractAnnotationTest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.callbacks.Callback;
+import io.swagger.v3.oas.annotations.callbacks.Callbacks;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import static org.testng.Assert.assertEquals;
 
 public class ApiResponseTypeSchemaTicket3169Test extends AbstractAnnotationTest {
 
@@ -62,6 +64,10 @@ public class ApiResponseTypeSchemaTicket3169Test extends AbstractAnnotationTest 
 
     }
 
+    class AnotherModel {
+
+    }
+
     @Test
     public void testApiResponseTypeVoidSchemaTicket3169Test() throws Exception {
         String expectedYaml = "openapi: 3.0.1\n" +
@@ -96,6 +102,110 @@ public class ApiResponseTypeSchemaTicket3169Test extends AbstractAnnotationTest 
         @GET
         @Path("/path")
         public void simpleGet(String data) {
+        }
+    }
+
+    @Test
+    public void testApiResponseTypeInApiResponsesSchemaTicket3169Test() throws Exception {
+        String expectedYaml = "openapi: 3.0.1\n" +
+                "paths:\n" +
+                "  /path:\n" +
+                "    get:\n" +
+                "      summary: Op\n" +
+                "      description: 'Response return a Void Type '\n" +
+                "      operationId: getWithNoParameters\n" +
+                "      requestBody:\n" +
+                "        content:\n" +
+                "          '*/*':\n" +
+                "            schema:\n" +
+                "              type: string\n" +
+                "      responses:\n" +
+                "        200:\n" +
+                "          description: voila!\n" +
+                "          content:\n" +
+                "            '*/*':\n" +
+                "              schema:\n" +
+                "                $ref: '#/components/schemas/MyModel'\n" +
+                "        400:\n" +
+                "          description: voila!\n" +
+                "          content:\n" +
+                "            '*/*':\n" +
+                "              schema:\n" +
+                "                $ref: '#/components/schemas/AnotherModel'\n" +
+                "components:\n" +
+                "  schemas:\n" +
+                "    MyModel:\n" +
+                "      type: object\n" +
+                "    AnotherModel:\n" +
+                "      type: object\n";
+        compareAsYaml(ResponseWithTypeInApiResponses.class, expectedYaml);
+    }
+
+    static class ResponseWithTypeInApiResponses {
+        @Operation(
+                summary = "Op",
+                description = "Response return a Void Type ",
+                operationId = "getWithNoParameters")
+        @GET
+        @ApiResponses(value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "voila!",
+                        type = MyModel.class),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "voila!",
+                        type = AnotherModel.class),
+        })
+        @Path("/path")
+        public void simpleGet(String data) {
+        }
+    }
+
+    @Test
+    public void simpleCallbacksWithTypeInApiResponse() {
+        String openApiYAML = readIntoYaml(ApiResponseTypeSchemaTicket3169Test.SimpleCallbacksTestWithApiReponseType.class);
+        int start = openApiYAML.indexOf("get:");
+        int end = openApiYAML.length() - 1;
+
+        String expectedYAML = "get:\n" +
+                "      summary: Simple get operation\n" +
+                "      operationId: getWithNoParameters\n" +
+                "      responses:\n" +
+                "        200:\n" +
+                "          description: voila!\n" +
+                "          content:\n" +
+                "            '*/*':\n" +
+                "              schema:\n" +
+                "                $ref: '#/components/schemas/MyModel'\n" +
+                "      callbacks:\n" +
+                "        testCallback1:\n" +
+                "          localhost:9080/airlines/reviews/{id}/1: {}\n" +
+                "components:\n" +
+                "  schemas:\n" +
+                "    MyModel:\n" +
+                "      type: object";
+        String extractedYAML = openApiYAML.substring(start, end);
+
+        assertEquals(extractedYAML, expectedYAML);
+    }
+
+    static class SimpleCallbacksTestWithApiReponseType {
+        @Callbacks({
+                @Callback(name = "testCallback1", operation = @Operation(), callbackUrlExpression = "localhost:9080/airlines/reviews/{id}/1")
+        })
+        @Operation(
+                summary = "Simple get operation",
+                operationId = "getWithNoParameters",
+                responses = {
+                        @ApiResponse(
+                                responseCode = "200",
+                                description = "voila!",
+                                type = MyModel.class)
+                })
+        @GET
+        @Path("/path")
+        public void simpleGet() {
         }
     }
 }
